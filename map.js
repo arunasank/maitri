1236c20d29b0b544e7a2de90342232264d499e1c
arunasank <aruna@mapbox.com>
2017-03-18 10:28:19 -0700

    update style

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ1bmFzYW5rIiwiYSI6ImRKNlNQa3MifQ.SIx-g-J1oWWlP4grTXopcg';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/arunasank/ciyf36uez00012ss4jkeus5hh', //hosted style id
+    style: 'mapbox://styles/arunasank/cj0fiafqr001y2rs424a14w63', //hosted style id
     center: [80.22979, 13.01417], // starting position
     zoom: 16.16, // starting zoom
     hash: true
